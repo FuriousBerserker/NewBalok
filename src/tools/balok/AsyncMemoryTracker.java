dc7c90f301fb509967290a5f4b3ccd2710b3f968
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-11-21 11:49:43 -0500

    Forgot a curly brace.

--- src/tools/balok/AsyncMemoryTracker.java
+++ src/tools/balok/AsyncMemoryTracker.java
@@ -43,6 +43,7 @@ public class AsyncMemoryTracker implements MemoryTracker {
             if (currentFrame.isFull()) {
                 queue.add(currentFrame.build());
             }
+        }
     }
 
     @Override
