55afa4ba40f248f271971ac43610e82d87474ca6
Lechen Yu <ylcmen@gmail.com>
2019-03-24 01:31:59 -0400

    update generateAccessFile.sh to allow specifying benchmarks through input

--- scripts/generateAccessFile.sh
+++ scripts/generateAccessFile.sh
@@ -6,12 +6,16 @@ CURRENT=$(pwd)
 BMS="crypt:lufact:moldyn:montecarlo:raytracer:series:sor:sparsematmult:avrora:batik:fop:h2:jython:luindex:lusearch:pmd:sunflow:tomcat:xalan"
 export PATH="${ROOT}/../build/bin:${PATH}"
 
+if [ ! -z $1 ]; then
+  BMS=$1
+fi
+echo $BMS
 cd ${ROOT}/../benchmarks
 echo $BMS | tr ':' '\n' | while read bm; do
     cd $bm
     echo " " $bm
     logFolder="${bm}"
-    ./TEST_BENCH -tool=BalokFE -noxml -quiet -noTidGC -folder=${logFolder} 2>&1
+    ./TEST_BENCH -tool=BalokFE -noxml -quiet -noTidGC -maxTid=10240 -folder=${logFolder} 2>&1
     mv ${logFolder} ${CURRENT}/
     cd ..
 done
